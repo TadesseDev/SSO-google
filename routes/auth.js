7189f263fd9e87fd300ae20ed61d4362af03db4a
tadessedev <tadessedev@gmail.com>
2024-08-19 20:15:24 +0300

    install passport and google-oidc packages

--- routes/auth.js
+++ routes/auth.js
@@ -3,9 +3,13 @@
 var express = require("express");
 
 var router = express.Router();
+var passport = require("passport");
+const GoogleStrategy = require("passport-google-oidc");
+const db = require("../db");
 
 router.get("/login", function (req, res, next) {
   res.render("login");
 });
 
+router.get("/login/federated/google", passport.authenticate("google"));
 module.exports = router;
